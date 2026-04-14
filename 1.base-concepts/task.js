99c64a5ee9ad366edb5e335205f89fea6e3f8266
Dmitriy Potykalov <dmitriy.potykalov@icloud.com>
2026-04-14 20:31:35 +0300

    lesson 1: initial commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,34 @@
-"use strict"
+'use strict';
+
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  const d = b ** 2 - 4 * a * c;
+
+  if (d > 0) {
+    arr.push((-b + Math.sqrt(d)) / (2 * a));
+    arr.push((-b - Math.sqrt(d)) / (2 * a));
+  }
+
+  if (d === 0) {
+    arr.push(-b / (2 * a));
+  }
+
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  const monthlyRate = percent / 100 / 12;
+  const loanBody = amount - contribution;
+
+  if (loanBody <= 0) {
+    return 0;
+  }
+
+  const monthlyPayment =
+    loanBody *
+    (monthlyRate + monthlyRate / ((1 + monthlyRate) ** countMonths - 1));
+
+  const totalAmount = monthlyPayment * countMonths;
+
+  return Number(totalAmount.toFixed(2));
 }
